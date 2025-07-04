0017f03dbbcf29425b43c98096a03e89945e42da
hiren prajapati <hirenprajapati7534@gmail.com>
2025-07-05 00:55:18 +0530

    Add alert message in usePrivateRoute custom hook

--- src/hooks/usePrivateRoutes.js
+++ src/hooks/usePrivateRoutes.js
@@ -1,13 +1,15 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const usePrivateRoutes = () => {
-  const token = localStorage.getItem('authToken');
+  const token = localStorage.getItem("authToken");
   const navigate = useNavigate();
 
   useEffect(() => {
-    if(token == null) {
-        navigate('/');
+    if (token == null) {
+      navigate("/");
+      Swal.fire('You need to login or signup to access this page');
     }
   }, [token, navigate]);
 
